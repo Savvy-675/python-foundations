1de6df344805432e248ff7d42868ec02fddb2b9d
Samarth Shete <158153067+Savvy-675@users.noreply.github.com>
2025-12-28 21:48:33 +0530

    Day 1: Python basics – variables, I/O, operators, conditionals

---
+++ day01_basics/i[_op_ifel.py
@@ -0,0 +1,13 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+print(f"My name is {name} and I am {age} years old.")
+
+# Fixed the indentation below:
+marks = int(input("Enter marks: "))
+
+if marks >= 40:
+    print("Pass")
+else:
+    print("Fail")
+
+print(f"{name} has {marks} marks")
